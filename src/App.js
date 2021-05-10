e0621305110c920a3b31c40cbe0eb35ea2502a78
Tuan Le Minh (SM) <deadlove011011@gmail.com>
2021-05-11 01:09:08 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, Image } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid divided="vertically">
+        <Grid.Row columns={3}>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+          <Grid.Column>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
